d580d13fc3d320de3af9a093edfd2c03c3a1c156
Alyssa K <aklein507@gmail.com>
2020-11-04 12:20:16 -0500

    added a function to check for the required packages and then install any that are not installed; also added comments for what each part is doing so far

--- ui.R
+++ ui.R
@@ -1,10 +1,38 @@
-library(shiny)
 
+#Sherlock-Genome: A R Shiny App for Genomic Analysis and Visualization
+
+# specify the packages needed for the app through a character vector
+# keep adding to this vector when additional packages are required to run the app successfully
+packages_req <- c("shiny","shinydashboard")
+
+# check for required packages and install those not installed
+# lapply() function to use the packages_req vector and carry out the function written
+pkg_check_fcn <-lapply(packages_req,function(x){
+# if a package in the packages_req vector is not installed, install it
+    if(!(x %in% installed.packages())){
+      install.packages(x, dependencies = TRUE, repos=structure(c(CRAN="http://cloud.r-project.org/")))
+# load the package after installing it
+      library(x, character.only = TRUE)
+# else statement for when the packages are already installed
+    }else{
+# load the package already installed into the workspace
+      library(x, character.only = TRUE)
+      # print(paste0(x,":" , "Package already installed"))
+    }
+
+})
+
+# ui setup 
 ui <- fluidPage(
+# title at the top of the page; windowTitle = what the browser tab reads
   titlePanel(title = "Sherlock-Genome: A R Shiny App for Genomic Analysis and Visualization",
              windowTitle = "Sherlock-Genome"),
+
+  # dashboard setup with left sidebar
   dashboardPage(skin = "blue",
     dashboardHeader(title = "Sherlock-Genome"),
+    
+# each of the following will be a tab on the left sidebar
     dashboardSidebar(sidebarMenu(
       menuItem("Study Overview", tabName = "study_overview"),
       menuItem("Data QC", tabName = "data_qc"),
@@ -16,6 +44,7 @@ ui <- fluidPage(
       menuItem("Survival Analysis", tabName = "survival_analysis"),
       menuItem("Integrative Analysis", tabName = "integrative_analysis")
     )),
+# each of the following are what the tabs assigned page will read when the tab is clicked
     dashboardBody(
       tabItems(tabItem(tabName = "study_overview", h2("Study Overview")),
                tabItem(tabName = "data_qc", h2("Data Quality Control (QC)")),
